4175cdf669d884f0f77e4b32d41f31a6086bfa39
Kevin S. Soares <kevin.soares@hotmail.com>
2022-03-07 17:42:10 -0300

    Adicionado navbar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Container from "./stateful/Container";
+import Navbar from "./stateless/Navbar";
+import Footer from "./stateless/Footer";
 
 
 function App() {
   return (
     <div>
-      <Container/>
+      <Navbar/>
+      <div className="container">
+        <Container/>
+        <Footer/>
+      </div>
     </div>
   );
 }
