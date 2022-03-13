7cfad3d574e1aa08a8244df02338f441f27bf85a
Kevin S. Soares <kevin.soares@hotmail.com>
2022-03-13 19:52:20 -0300

    Corrigido problema no navbar

--- src/stateless/Navbar.js
+++ src/stateless/Navbar.js
@@ -1,21 +1,19 @@
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container container-fluid">
-                
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mb-2 mb-lg-0">
-                        <li className="nav-item" id="tool">
-                            <a className="nav-link" href={window.location.origin}><strong>Ferramenta</strong></a>
-                        </li>
-                        <li className="nav-item" id="about">
-                            <a className="nav-link" href={window.location.origin + "/About"}><strong>Sobre</strong></a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler ms-1" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="container navbar-nav mr-auto mt-2 mt-lg-0 ps-1">
+                    <li className="nav-item">
+                        <a className="nav-link" href={window.location.origin}><strong>Ferramenta</strong></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href={window.location.origin + "/About"}><strong>Sobre</strong></a>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
